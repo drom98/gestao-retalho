97ab3ef01f55f495a2a87db1fedaa7cca9f795b3
drom98 <diogoomarques98@gmail.com>
2020-03-26 20:04:43 +0000

    Novo layout para login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,51 +2,33 @@
 
 @section('content')
 <div class="container">
-    <!-- Outer Row -->
-    <div class="row justify-content-center align-items-center">
-      <div class="col-xl-6 col-lg-6 col-md-4">
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Iniciar Sessão</h1>
-                  </div>
-                    <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="form-group">
-                      <input name="username" type="text" class="form-control form-control-user @error('username') is-invalid @enderror" value="{{ old('username') }}" required autocomplete="username" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter username...">
-                    @error('username')
-                      <span class="invalid-feedback" role="alert">
-                          <strong>{{ $message }}</strong>
-                      </span>
-                    @enderror
-                    </div>
-                    <div class="form-group">
-                      <input name="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="exampleInputPassword" placeholder="Password" required autocomplete="current-password">
-
-                    @error('password')
-                      <span class="invalid-feedback" role="alert">
-                          <strong>{{ $message }}</strong>
-                      </span>
-                    @enderror
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
+    <form class="form-signin" method="POST" action="{{ route('login') }}">
+        @csrf
+        <img class="mb-4" src="#" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Iniciar Sessão</h1>
+        <label for="username" class="sr-only">Utilizador</label>
+        <input name="username" type="text" class="form-control form-control-user @error('username') is-invalid @enderror" value="{{ old('username') }}" required autocomplete="username" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Utilizador">
+        @error('username')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input name="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="exampleInputPassword" placeholder="Password" required autocomplete="current-password">
+        @error('password')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+        <!--
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
         </div>
-      </div>
-    </div>
+        -->
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+        <p class="mt-5 mb-3 text-muted">Aplicação de gestão de retalho</p>
+    </form>
 </div>
 @endsection
--- resources/views/dashboard/layouts/login.blade.php
+++ resources/views/dashboard/layouts/login.blade.php
@@ -3,36 +3,20 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin 2 - Blank</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="{{asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/signin.css') }}">
+    <title>Gestão de Retalho - Login</title>
 
 </head>
 
-<body id="page-top">
+<body class="text-center">
 
     @yield('content')
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="{{asset('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="{{asset('assets/js/sb-admin-2.min.js')}}"></script>
 
 </body>
 
