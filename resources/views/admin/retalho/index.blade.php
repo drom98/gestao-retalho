9c37a702d55ab4196b142a23b2e38d73f5bc620c
Diogo Oliveira <diogoomarques98@gmail.com>
2020-07-10 23:03:13 +0100

    Dashboard: index retalho - layout pesquisa avançada

--- resources/views/admin/retalho/index.blade.php
+++ resources/views/admin/retalho/index.blade.php
@@ -10,9 +10,59 @@
                     {{ \App\Retalho::count() }}
                 </span>
             </h1>
-        </div>
-        <div class="col" style="text-align: right">
-            <a href="{{ route('admin.retalho.create') }}" class="btn btn-sm btn-dark">Adicionar novo</a>
+
+            <div class="row mb-4">
+                <div class="col-md-2">
+                    <a href="{{ route('admin.retalho.create') }}" class="btn btn-sm btn-block btn-dark">Adicionar novo</a>
+                </div>
+                <div class="col-md-2">
+                    <button class="btn btn-sm btn-block btn-secondary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        Pesquisa avançada
+                    </button>
+                </div>
+            </div>
+
+            <div class="collapse" id="collapseExample">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col">
+                                    <label for="lote">Lote:</label>
+                                    <input type="text" class="form-control form-control-sm" id="lote">
+                                </div>
+                                <div class="form-group col">
+                                    <label for="comprimento">Comprimento:</label>
+                                    <input type="number" class="form-control form-control-sm" id="comprimento">
+                                </div>
+                                <div class="form-group col">
+                                    <label for="largura">Largura:</label>
+                                    <input type="number" class="form-control form-control-sm" id="largura">
+                                </div>
+                                <div class="form-group col">
+                                    <label for="area">Área:</label>
+                                    <input type="number" class="form-control form-control-sm" id="area">
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col">
+                                    <label for="tipoVidro">Tipo de vidro:</label>
+                                    <select name="tipoVidro" id="tipoVidro"></select>
+                                </div>
+                                <div class="form-group col">
+                                    <label for="localizacao">Localização:</label>
+                                    <select name="localizacao" id="input_localizacao"></select>
+                                </div>
+                                <div class="form-group col">
+                                    <label for="utilizador">Utilizador:</label>
+                                    <select name="utilizador" id="utilizador"></select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
@@ -61,6 +111,9 @@
     <script !src="">
         $(document).ready(function() {
             $('.seccaoSelect').select2({theme: 'bootstrap4'});
+            $('#tipoVidro').select2({theme: 'bootstrap4'});
+            $('#input_localizacao').select2({theme: 'bootstrap4'});
+            $('#utilizador').select2({theme: 'bootstrap4'});
         });
     </script>
 @endpush
