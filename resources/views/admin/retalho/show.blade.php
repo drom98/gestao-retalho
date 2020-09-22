2b5146d8f0b259ab3f73f3609a5d389d214b18bc
Diogo Oliveira <diogoomarques98@gmail.com>
2020-09-22 22:35:25 +0100

    Área admin: layout página de retalho

--- resources/views/admin/retalho/show.blade.php
+++ resources/views/admin/retalho/show.blade.php
@@ -1,10 +1,9 @@
 @extends('admin.dashboard.layouts.app')
 
 @section('content')
-    <h1 class="h3 mb-4 text-gray-800">Retalho #{{ $retalho->id }}</h1>
-    @error('comprimento')
-    <p>{{ $message }}</p>
-    @enderror
+
+    @inject('data', 'App\Helpers\Helper');
+
     @if ($errors->any())
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             @foreach ($errors->all() as $error)
@@ -15,37 +14,30 @@
             </button>
         </div>
     @endif
-    <div class="row">
-        <div class="col-md-8">
-            <div class="card mb-4">
-                <div class="card-header">
-                    Editar retalho nº {{ $retalho-> id }}
-                </div>
+
+    <div class="row mb-4">
+        <div class="col-md-12">
+            <div class="card">
                 <div class="card-body">
-                    <form class="user" method="POST" action="{{ route('admin.retalho.update', $retalho->id) }}">
-                        @csrf
-                        @method('PATCH')
-                        <div class="form-group">
-                            <label for="lote">Lote:</label>
-                            <input name="lote" id="lote" type="text" class="form-control" placeholder="Lote" value="{{ $retalho->num_lote }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="comprimento">Comprimento(mm):</label>
-                            <input name="comprimento" id="comprimento" step="1" type="number" class="form-control" placeholder="Comprimento(mm)" value="{{ $retalho->comprimento }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="largura">Largura(mm):</label>
-                            <input name="largura" id="largura" step="1" type="number" class="form-control" placeholder="Largura(mm)" value="{{ $retalho->largura }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="area">Área</label>
-                            <input value="{{ $retalho->area }}" type="number" step="0.01" name="area" id="area" class="form-control" readonly>
-                        </div>
-                        <a href="{{ URL::previous() }}" class="btn btn-secondary">Cancelar</a>
-                        <button type="submit" class="btn btn-success">Guardar</button>
-                    </form>
+                    <h5 class="font-weight-bold">Retalho #{{ $retalho->id }}</h5>
+                    <small>{{ $data->getLocalizedDate($retalho) }}</small>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8">
+            <ul class="list-group">
+                <li class="list-group-item"><strong class="d-inline-block w-50">Tipo de vidro: </strong><span class="text-right">{{ $retalho->tipoVidro->nome }}</span></li>
+                <li class="list-group-item"><strong class="d-inline-block w-50">Comprimento: </strong><span class="text-right">{{ $retalho->comprimento }}</span></li>
+                <li class="list-group-item"><strong class="d-inline-block w-50">Largura: </strong><span class="text-right">{{ $retalho->largura }}</span></li>
+                <li class="list-group-item"><strong class="d-inline-block w-50">Área: </strong><span class="text-right">{{ $retalho->area }}</span></li>
+                <li class="list-group-item"><strong class="d-inline-block w-50">Lote: </strong><span class="text-right">{{ $retalho->num_lote }}</span></li>
+                <li class="list-group-item"><strong class="d-inline-block w-50">Utilizador: </strong><span class="text-right">{{ $retalho->retalhable->name }}</span></li>
+                <li class="list-group-item"><strong class="d-inline-block w-50">Data arrumação: </strong><span class="text-right">{{ $data->getLocalizedDate($retalho) }}</span></li>
+                <li class="list-group-item"><strong class="d-inline-block w-50">Local arrumação: </strong><span class="text-right">{{ $retalho->localizacao->nome }}</span></li>
+            </ul>
         </div>
     </div>
 
